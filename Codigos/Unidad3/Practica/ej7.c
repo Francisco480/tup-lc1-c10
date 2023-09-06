2ac193e79e02cdbda6cd6792acf45a223bb31988
Francisco Poli Veliz <polivelizfrancisco@gmail.com>
2023-09-06 18:39:46 -0300

    subida archivos

--- Codigos/Unidad3/Practica/ej7.c
+++ Codigos/Unidad3/Practica/ej7.c
@@ -1,12 +1,9 @@
-<<<<<<< HEAD
 /*Ingresar el valor de una venta y aplicar el 15% de descuento si el valor es >= 100000 y
 un 10% si es menor. Validar que la venta sea mayor que 0, caso contrario informar
 dicha situación.*/
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <wchar.h>
-#include <locale.h>
 #include <math.h>
 
 int main()
@@ -14,8 +11,7 @@ int main()
     
     float venta, total;
     total = 0;
-    setlocale(LC_ALL, "es_ES"); 
-    printf("Ingrese el valór de la venta realizada\n");
+        printf("Ingrese el valor de la venta realizada\n");
     scanf("%f", &venta);
 
     if (venta < 0)
@@ -39,45 +35,4 @@ int main()
 
     system("pause");
     return 0;
-=======
-/*Ingresar el valor de una venta y aplicar el 15% de descuento si el valor es >= 100000 y 
-un 10% si es menor. Validar que la venta sea mayor que 0, caso contrario informar 
-dicha situación.*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <math.h>
-
-int main (){
-
-    float venta,  total;
-    total= 0;
-
-    printf ("Ingrese el valor de la venta realizada\n");
-    scanf ("%f", &venta);
-
-    if (venta<0)
-    {
-        printf ("Valor ingresado invalido\n");
-    }
-    if (venta>0 & venta<100000)
-    {
-        total = venta - (venta * 0.10);
-        printf ("El total es: %f\n",total);
-    }
-    else if (venta>=100000)
-    {
-        total = venta - (venta * 0.15);
-        printf ("El total es: %f\n",total); 
-    }
-    else 
-    {
-        printf ("Valor ingresado invalido\n");
-    }
-    
-
-
-    system("pause");
-    return 0;
->>>>>>> fa8d99837018da2bb3a0a1535f403ba21fbf4894
 }
